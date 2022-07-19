ca0f9b0ef7c958ee541e4b91afca363890b46aa3
ijo42 <admin@Ijo42.ru>
2022-07-19 22:20:17 +0500

    feat: new statistic catch e.g. passing score, total applicants

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 bot = interactions.Client(token=os.getenv('BOT_TOKEN'))
 guild_id = int(os.getenv("GUILD_ID"))
 webhook_url = os.getenv("WEBHOOK_URL")
-file_name = "subscribed.json"
+file_name = "store/subscribed.json"
 sleep_time = 3600
 list_link = "http://www.psu.ru/files/docs/priem-2022/"
 
@@ -19,7 +19,7 @@ if os.path.exists(file_name):
     with open(file_name, "r") as read_file:
         data = json.loads(read_file.read())
 else:
-    data = {'last_list_size': -1, 'abit': {}}
+    data = {'last_list_size': -1, 'abit': {}, "spec": {}}
 
 
 def save():
@@ -70,24 +70,36 @@ def checkList():
         req_text = requests.get(list_link)
         req_text.encoding = req_text.apparent_encoding
         req_text = req_text.text
+        time = re.search(re.compile("Сформировано(.*)<"), req_text)[1].strip()
         soup = BeautifulSoup(req_text, "html.parser")
+        base_spec = {}
 
         for dis_id in data['abit'].items():
-            new_val = find_by_id(soup, data['abit'][dis_id[0]]['id'])
-            txt = f"<@{dis_id[0]}>, опубликовано новое изменение!"
-            for spec in new_val.items():
-                txt += f'''\nНаправление: {spec[0]}
-Место в списке: {spec[1]['place']} / {spec[1]['admission_number_digit']}
-'''
-                if 'prev' not in data['abit'][dis_id[0]]:
-                    data['abit'][dis_id[0]]['prev'] = {}
+            new_val = parse_place_by_id(soup, data['abit'][dis_id[0]]['id'])
+            txt = f"<@{dis_id[0]}>, опубликовано новое изменение! ({time})"
 
-                if spec[0] in data['abit'][dis_id[0]]['prev']:
-                    change = data['abit'][dis_id[0]]['prev'][spec[0]]["place"] - new_val[spec[0]]["place"]
-                    if change != 0:
-                        txt += f'Изменение: {"+" if change > 0 else "-"}{abs(change)} ' + (":arrow_up:" if change > 0
+            if 'prev' not in data['abit'][dis_id[0]]:
+                data['abit'][dis_id[0]]['prev'] = {}
+
+            for spec in new_val.items():
+                is_spec_in_prev = spec[0] in data['abit'][dis_id[0]]['prev']
+                if spec[0] not in base_spec:
+                    base_spec[spec[0]] = parse_spec(soup, spec[0])
+                spec_parsed = base_spec[spec[0]]
+                txt += f'''\nНаправление: *{spec[0]}*
+Проходной балл: {spec_parsed['passing_score']}
+КЦП: {spec_parsed['admission_digits'][0]} / {spec_parsed['admission_digits'][1]}; ОП: {spec_parsed['admission_digits'][2]} 
+Количество заявлений: {spec_parsed['total_applicants']}
+Количество заявлений на место: {round(spec_parsed['total_applicants'] / spec_parsed['admission_digits'][0], 2)}
+**Место в списке: {spec[1]}**
+'''
+                if is_spec_in_prev:
+                    place_change = data['abit'][dis_id[0]]['prev'][spec[0]] - new_val[spec[0]]
+                    if place_change != 0:
+                        txt += f'Изменение: {"+" if place_change > 0 else "-"}{abs(place_change)} ' + (":arrow_up:" if place_change > 0
                                                                                            else ":arrow_down:") + "\n"
                 data['abit'][dis_id[0]]['prev'][spec[0]] = spec[1]
+                data['spec'][spec[0]] = spec_parsed
             d_val = {'content': txt}
             if len(new_val.items()) > 0:
                 requests.post(webhook_url, json=d_val)
@@ -95,19 +107,24 @@ def checkList():
         data["last_list_size"] = current_list_size
         save()
 
+def parse_spec(soup, spec_name):
+    spec_element = soup.find("span", string=spec_name).parent.parent
+    admission_digits = list(int(k.text) for k in spec_element.find_all("strong") if k.text.isdigit())[:8]
 
-def find_by_id(soup, id):
-    speced = {}
+    return {
+        "admission_digits": admission_digits,
+        "passing_score": int(spec_element.find("td", string=admission_digits[0]).parent.find_all("td")[-1].text),
+        "total_applicants": int(spec_element.find("table").find_all("tr")[-1].find("td").text),
+    }
+
+
+def parse_place_by_id(soup, id):
+    places = {}
     for spec in soup.find_all("font", string=id):
         _parent = spec.parent.parent
-        place = int(_parent.find("td").text)
         spec_name = _parent.parent.parent.find("h2").find_all("span")[2].get_text()
-        admission_number_digit = _parent.parent.parent.find_all("p")[2].get_text().strip().split(":")[1].split()[0]
-        speced[spec_name] = {
-            "place": place,
-            "admission_number_digit": admission_number_digit,
-        }
-    return speced
+        places[spec_name] = int(_parent.find("td").text)
+    return places
 
 
 def worker():
